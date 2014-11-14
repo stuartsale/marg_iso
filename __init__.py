c7aedf80998f005421d60bcfd13b92597d9b79c5
Stuart Sale <stuart.sale@gmail.com>
2014-11-14 17:31:48 +0000

    Removing obselete code

--- __init__.py
+++ __init__.py
@@ -82,143 +82,7 @@ class star_posterior:
         self.MCMC_run=False 
         self.best_gmm=None       
         
-    # initialise MCMC_chain
-    
-    def init_chain(self, chain_length):
-        self.last_Teff=3.8
-        self.last_logg=4.3
-        self.last_feh=0.
-        self.last_dist_mod=10.
-        self.last_logA=0.
-        
-        self.last_iso_obj=self.isochrones.query(self.last_feh, self.last_Teff, self.last_logg)
-        
-        self.set_lastprob()
-        self.set_lastprior()
-        
-        self.Teff_chain=np.zeros(chain_length)
-        self.logg_chain=np.zeros(chain_length)
-        self.feh_chain=np.zeros(chain_length)
-        self.dist_mod_chain=np.zeros(chain_length)
-        self.logA_chain=np.zeros(chain_length)
-
-        self.prob_chain=np.zeros(chain_length)
-        self.prior_chain=np.zeros(chain_length)
-        self.Jac_chain=np.zeros(chain_length)
-        
-        self.r_chain=np.zeros(chain_length)
-        self.i_chain=np.zeros(chain_length)
-        self.ha_chain=np.zeros(chain_length)
-        
-        self.itnum_chain=np.zeros(chain_length)                    
-        
-        
-        
-    # find likelihood of last param set
-    
-    def set_lastprob(self):
-        A=np.exp(self.last_logA)
-        self.last_prob=0.
-        self.last_prob=-(np.power(self.r-(self.last_iso_obj.r0+self.last_dist_mod+self.last_iso_obj.vr*A+self.last_iso_obj.ur*A*A) ,2)/(2*self.dr*self.dr)
-                        +np.power(self.i-(self.last_iso_obj.i0+self.last_dist_mod+self.last_iso_obj.vi*A+self.last_iso_obj.ui*A*A) ,2)/(2*self.di*self.di)
-                        +np.power(self.ha-(self.last_iso_obj.ha0+self.last_dist_mod+self.last_iso_obj.vha*A+self.last_iso_obj.uha*A*A) ,2)/(2*self.dha*self.dha ))
-            
-
-    #  find likelihood of test param set
-
-    def set_testprob(self):
-        A=np.exp(self.test_logA)
-        self.test_prob=0.
-        self.test_prob=-(np.power(self.r-(self.test_iso_obj.r0+self.test_dist_mod+self.test_iso_obj.vr*A+self.test_iso_obj.ur*A*A) ,2)/(2*self.dr*self.dr)
-                        +np.power(self.i-(self.test_iso_obj.i0+self.test_dist_mod+self.test_iso_obj.vi*A+self.test_iso_obj.ui*A*A) ,2)/(2*self.di*self.di)
-                        +np.power(self.ha-(self.test_iso_obj.ha0+self.test_dist_mod+self.test_iso_obj.vha*A+self.test_iso_obj.uha*A*A) ,2)/(2*self.dha*self.dha) )
-
-
-    # find prior prob of last param set
-    
-    def set_lastprior(self):
-        dist=pow(10., self.last_dist_mod/5.+1.)
-        R_gal=np.sqrt( 8000*8000+np.power(dist*self.cosb,2)-2*8000*dist*self.cosb*self.cosl )        
-        self.last_prior=np.log(self.last_iso_obj.Jac) + 3*np.log(dist) - dist/2500. -2.3*np.log(self.last_iso_obj.Mi) + 2.3026*self.last_iso_obj.logage + self.last_logA \
-                        -np.power(self.last_iso_obj.feh+(R_gal-8000.)*0.00007,2)/(2*0.0625) 
-        
-    # find prior prob of test param set
-    
-    def set_testprior(self):
-        dist=pow(10., self.test_dist_mod/5.+1.)
-        R_gal=np.sqrt( 8000*8000+np.power(dist*self.cosb,2)-2*8000*dist*self.cosb*self.cosl )
-        self.test_prior=np.log(self.test_iso_obj.Jac) + 3*np.log(dist) - dist/2500. -2.3*np.log(self.test_iso_obj.Mi) + 2.3026*self.test_iso_obj.logage + self.test_logA \
-                        -np.power(self.test_iso_obj.feh+(R_gal-8000.)*0.00007,2)/(2*0.0625) 
-    
-    # MCMC sampler
-    
-    def MCMC_Metropolis(self, Teff_prop, logg_prop, feh_prop, dist_mod_prop, logA_prop, iterations=10000, thin=1):
-    
-        self.init_chain(int(iterations/thin))
-        
-        self.accept=0
-        
-        for it in range(iterations):
-            # Set test params
-            
-            self.test_Teff=self.last_Teff + np.random.normal()*Teff_prop
-            self.test_logg=self.last_logg + np.random.normal()*logg_prop
-            self.test_feh=self.last_feh + np.random.normal()*feh_prop
-            self.test_dist_mod=self.last_dist_mod + np.random.normal()*dist_mod_prop
-            self.test_logA=self.last_logA + np.random.normal()*logA_prop
-            
-            try:
-                self.test_iso_obj=self.isochrones.query(self.test_feh, self.test_Teff, self.test_logg)    
-            except IndexError:
-                continue
-            
-            # get probs
-            
-            self.set_testprob()
-            self.set_testprior()
-            
-            # accept/reject
-            
-            threshold=np.log(np.random.uniform())
-            
-            if (self.test_prob-self.last_prob+self.test_prior-self.last_prior)>=threshold:
-                self.last_prob=self.test_prob
-                self.last_prior=self.test_prior
-
-                self.last_Teff=self.test_Teff
-                self.last_logg=self.test_logg
-                self.last_feh=self.test_feh
-                self.last_dist_mod=self.test_dist_mod
-                self.last_logA=self.test_logA
-                        
-                self.last_iso_obj=self.test_iso_obj
-                
-                self.accept+=1
-                
-            
-            # store in chain 
-            
-            if it%thin==0:   
-                self.Teff_chain[it/thin]=self.last_Teff
-                self.logg_chain[it/thin]=self.last_logg
-                self.feh_chain[it/thin]=self.last_feh
-                self.dist_mod_chain[it/thin]=self.last_dist_mod
-                self.logA_chain[it/thin]=self.last_logA 
-
-                self.prob_chain[it/thin]=self.last_prob 
-                self.prior_chain[it/thin]=self.last_prior 
-                self.Jac_chain[it/thin]=self.last_iso_obj.Jac
-                
-                A=np.exp(self.last_logA)                
-                self.r_chain[it/thin]=self.last_iso_obj.r0+self.last_dist_mod+self.last_iso_obj.vr*A+self.last_iso_obj.ur*A*A
-                self.i_chain[it/thin]=self.last_iso_obj.i0+self.last_dist_mod+self.last_iso_obj.vi*A+self.last_iso_obj.ui*A*A
-                self.ha_chain[it/thin]=self.last_iso_obj.ha0+self.last_dist_mod+self.last_iso_obj.vha*A+self.last_iso_obj.uha*A*A
-                
-                self.itnum_chain[it/thin]=it
-                
-                if it!=0 and it%10000==0:
-                    print self.accept*1./(it+1)
-                    
+                  
                     
     # ==============================================================
     # Functions to work with emcee sampler
@@ -501,6 +365,7 @@ class star_posterior:
 
         
         plt.show()
+        
 
 
 # class to store clusters in posterior space
